3e065c9142d70d40885df1b96eb3f22a709e2b7a
Piotr Szwarc <quarcu@gmail.com>
2018-04-11 23:41:46 +0200

    Python: array modify last element, join with comma and space

--- python/comma_code.py
+++ python/comma_code.py
@@ -2,8 +2,11 @@
 
 
 def comma_code(arr):
-    arr[-1] = 'and ' + arr[-1]
-    print(', '.join(arr))
+    if len(arr) >= 2:
+        arr[-1] = 'and ' + arr[-1]
+        print(', '.join(arr))
+    else:
+        print("Array is too short")
 
 
 spam = ['apples', 'bananas', 'tofu', 'cats']
