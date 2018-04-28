fc6409dc04efe9a4eb0ae54e4aa063e27be3246a
Piotr Szwarc <quarcu@gmail.com>
2018-04-28 14:10:36 +0200

    Compare list and dict unordered.

---
+++ python/dict_list.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3
+
+spam = ['cats', 'dogs', 'moose']
+bacon = ['dogs', 'moose', 'cats']
+print(spam)
+print(bacon)
+print('spam == bacon: ' + str(spam == bacon))
+
+print()
+
+eggs = {'name': 'Zophie', 'species': 'cat', 'age': '8'}
+ham = {'species': 'cat', 'age': '8', 'name': 'Zophie'}
+print(eggs)
+print(ham)
+print('eggs == ham: ' + str(eggs == ham))
