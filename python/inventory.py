6d63e2d79686463965959283771ce31908bab699
Piotr Szwarc <quarcu@gmail.com>
2018-04-28 15:26:12 +0200

    List to Dictionary Function for Fantasy Game Inventory

--- python/inventory.py
+++ python/inventory.py
@@ -10,4 +10,19 @@ def displayInventory(inventory):
         item_total += v
     print("Total number of items: " + str(item_total))
 
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        inventory.setdefault(item, 0)
+        inventory[item] += 1
+    return inventory
+
 displayInventory(stuff)
+
+print("---------------------------------------------------")
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+
+displayInventory(inv)
+
