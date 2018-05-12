fff00dcc3cbc816160109bd1947eb5041bfdf19e
Piotr Szwarc <quarcu@gmail.com>
2018-05-12 13:36:56 +0200

    Updated nested_dict according to styling guide

--- python/nested_dict.py
+++ python/nested_dict.py
@@ -1,8 +1,9 @@
 #!/usr/local/bin/python3
 
 allGuests = {'Alice': {'apples': 5, 'pretzels': 12},
-            'Bob': {'ham sandwiches': 3, 'apples': 2},
-            'Carol': {'cups': 3, 'apple pies': 1}}
+             'Bob': {'ham sandwiches': 3, 'apples': 2},
+             'Carol': {'cups': 3, 'apple pies': 1}}
+
 
 def totalBrought(guests, item):
     numBrought = 0
@@ -10,12 +11,14 @@ def totalBrought(guests, item):
         numBrought = numBrought + v.get(item, 0)
     return numBrought
 
+
 def items(guests):
     allItems = {}
     for guestName, guestItems in guests.items():
         for guestItem in guestItems.keys():
             allItems.setdefault(guestItem, 0)
     return allItems
+
 
 print('Number of things being brought:')
 for item in items(allGuests):
