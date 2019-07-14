83fd116d08b054a56def971d78f12f1adcbd572b
Piotr Szwarc <quarcu@gmail.com>
2019-07-14 12:11:48 +0200

    Folium: added 2 groups: volcanoes and population. Added world.json for data

--- python/proj2/map.py
+++ python/proj2/map.py
@@ -24,16 +24,33 @@ Volcano name:<br>
 Height: %s m
 """
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location=[lt, ln],
+    fgv.add_child(folium.CircleMarker(location=[lt, ln],
                                      popup=folium.Popup(iframe, parse_html=True),
                                      fill_opacity=0.7,
                                      fill=True,
                                      color = 'grey',
                                      fill_color=color_producer(el)))
 
-map.add_child(fg)
+fgp = folium.FeatureGroup(name="Population")
+with open('world.json', 'r', encoding='utf-8-sig') as file:
+    data = file.read()
+    fgp.add_child(folium.GeoJson(
+            data=data,
+            style_function=lambda x: {
+                'fillColor':
+                    'green' if x['properties']['POP2005'] < 10000000
+                    else 'orange' if x['properties']['POP2005'] < 20000000
+                    else 'red'
+            }
+        )
+    )
+
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
