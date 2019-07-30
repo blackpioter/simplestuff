4961e8e9ee55b633382c6aa15b0ba922600ea9cb
Piotr Szwarc <quarcu@gmail.com>
2019-07-30 18:31:30 +0200

    Python inheritance

--- python/proj5-desktop-app/account/acc.py
+++ python/proj5-desktop-app/account/acc.py
@@ -15,11 +15,32 @@ class Account:
         with open(self.filepath, 'w') as file:
             file.write(str(self.balance))
 
-account=Account("balance.txt")
-print(account.balance)
-account.withdraw(100)
-account.commit()
-print(account.balance)
-account.deposit(200)
-account.commit()
-print(account.balance)
+class Checking(Account):
+    """This class generates checking account objects"""
+
+    type = "Checking"
+
+    def __init__(self,filepath,fee):
+        Account.__init__(self,filepath)
+        self.fee=fee
+
+    def transfer(self,amount):
+        self.balance=self.balance - amount - self.fee
+
+# account=Account("balance.txt")
+# print(account.balance)
+# account.withdraw(100)
+# account.commit()
+# print(account.balance)
+# account.deposit(200)
+# account.commit()
+# print(account.balance)
+
+jack_checking=Checking("jack.txt",1)
+
+jack_checking.transfer(100)
+jack_checking.commit()
+print(jack_checking.balance)
+print(jack_checking.type)
+
+print(jack_checking.__doc__)
