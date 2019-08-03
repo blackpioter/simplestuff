a7be7e32dae18fda3ab209405f641279d322d3b5
Piotr Szwarc <quarcu@gmail.com>
2019-08-03 18:16:34 +0200

    Plotting script: read from motion_detector and graph activity with bokeh

---
+++ python/proj6-motion-detector/plotting.py
@@ -0,0 +1,21 @@
+from bokeh.plotting import figure, output_file, show
+from bokeh.layouts import column
+from bokeh.models import HoverTool, ColumnDataSource
+# from datetime import datetime as dt
+from motion_detector import df
+
+df["Start_string"]=df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_string"]=df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+cds=ColumnDataSource(df)
+
+p=figure(x_axis_type='datetime', height=100, width=500,title="Motion graph")
+p.yaxis.minor_tick_line_color=None
+p.ygrid[0].ticker.desired_num_ticks=1
+
+hover=HoverTool(tooltips=[("Start","@Start_string"),("End","@End_string")])
+p.add_tools(hover)
+
+q=p.quad(left="Start",right="End",bottom=0,top=1,color="green", source=cds)
+
+output_file("Graph.html")
+show(column(p,sizing_mode='stretch_both'))
